cd6d54043c854802467912c6cd85df98d089445a
monicakumbhar <monicakumbhar21@gmail.com>
2025-11-11 17:56:49 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,8 +1,27 @@
+variable "ami_id"{
+    description = "AMI ID value"
+    type = string
+    default = "ami-0157af9aea2eef346"
+}
+
+variable "instance_type" {
+    description = "Instance Type"
+    type = string
+    default = "t3.micro"
+  
+}
+
 provider "aws" {
     region = "us-east-1"  # Set your desired AWS region
 }
 
 resource "aws_instance" "example" {
-    ami           = "ami-0360c520857e3138f"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    ami           = var.ami_id  # Specify an appropriate AMI ID
+    instance_type = var.instance_type
+}
+
+output "public_ip" {
+    description = "AWS EC2 instance public IP"
+    value = aws_instance.example.public_ip
+  
 }
